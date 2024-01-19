7d53cbf24233e6e6a80e233ee1b6c0fed2cf39d9
Max de Groot <m.r.degroot-1@student.tudelft.nl>
2024-01-19 22:14:29 +0100

    Add linear fits

--- data-analysis/src/analyze_branches.py
+++ data-analysis/src/analyze_branches.py
@@ -5,6 +5,7 @@ import csv
 import os
 from typing import List
 
+import numpy as np
 from matplotlib import pyplot as plt
 from file_utils import save_figure
 from branch_measurement import BranchMeasurement, read_branch_measurements_from_row
@@ -29,6 +30,7 @@ def read_branch_measurements(file_path: str) -> List[BranchMeasurement]:
 def scatterplot_junctions_and_branches(branch_measurements: List[BranchMeasurement], file_name: str) -> None:
     """
     Generate scatterplot of junction count and branch count.
+    Also finds linear fit of points using numpy
 
     :param branch_measurements: List of branch measurements
     :param file_name: Name of original csv file
@@ -40,8 +42,21 @@ def scatterplot_junctions_and_branches(branch_measurements: List[BranchMeasureme
     plt.xlabel('Junctions')
     plt.ylabel('Branches')
 
+    # Fitting 1st degree polynomial
+    fit = np.polyfit(junction_counts, branch_counts, 1)
+
+    # y = ax + b
+    a = fit[0]
+    b = fit[1]
+
+    # Plot line
+    xs = [i for i in range(int(min(junction_counts)), int(max(junction_counts)))]
+    ys = [a * x + b for x in xs]
+    plt.plot(xs, ys, color='red')
+
     save_path = '../../resources/output/branch-analysis/' + file_name + ".png"
     save_figure(save_path)
+    plt.close()
 
 
 def generate_branch_measurement_graphs(branch_measurements: List[BranchMeasurement], file_name: str) -> None:
